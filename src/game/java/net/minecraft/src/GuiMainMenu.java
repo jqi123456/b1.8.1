fa319b8d8672d6f17185f91bea019adbda2f8606
Colbster937 <96893162+colbychittenden@users.noreply.github.com>
2025-11-02 14:25:09 -0600

    updaet btn pos

--- src/game/java/net/minecraft/src/GuiMainMenu.java
+++ src/game/java/net/minecraft/src/GuiMainMenu.java
@@ -91,8 +91,8 @@ public class GuiMainMenu extends GuiScreen {
 		if(false) {
 			this.controlList.add(new GuiButton(0, this.width / 2 - 100, var4 + 72, var2.translateKey("menu.options")));
 		} else {
-			this.controlList.add(new GuiButton(0, this.width / 2 - 100, var4 + 72 + 12, 98, 20, var2.translateKey("menu.options")));
-			this.controlList.add(new GuiButton(4, this.width / 2 + 2, var4 + 72 + 12, 98, 20, I18n.format("menu.editProfile")));
+			this.controlList.add(new GuiButton(0, this.width / 2 - 100, var4 + 72, 98, 20, var2.translateKey("menu.options")));
+			this.controlList.add(new GuiButton(4, this.width / 2 + 2, var4 + 72, 98, 20, I18n.format("menu.editProfile")));
 		}
 
 		if(this.mc.session == null) {
